01a63df44cfea4b8daf29a46b861e4fe66b4b1d7
Danu <dirajadanu@gmail.com>
2023-05-25 21:16:18 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pas/icons_dart_icons.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,51 +14,150 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 228, 224, 224),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Color.fromARGB(255, 240, 240, 240),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60, left: 30),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Login Account',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w700),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Icon(Icons.person_2_outlined),
+                          SizedBox(
+                            width: 110,
+                          ),
+                          Image.asset(
+                            'assets/ic_indonesia.png',
+                            width: 40,
+                          ),
+                          Icon(
+                            IconsDart.shape,
+                            size: 9,
+                          )
+                          // Icon()
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, left: 30),
+                      child: Text(
+                        'Hello , welcome back to our account !',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              child: Row(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(top: 40, left: 30),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Login Account',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w700),
+                    padding: const EdgeInsets.only(left: 30, top: 60),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(
+                          Size(150, 50),
                         ),
-                        SizedBox(
-                          width: 10,
+                        backgroundColor: MaterialStateProperty.all(
+                          Color(0xff6A00BF),
                         ),
-                        Icon(Icons.person_2_outlined),
-                        SizedBox(
-                          width: 110,
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(0),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(0),
+                          )),
                         ),
-                        Image.asset(
-                          'assets/ic_indonesia.png',
-                          width: 40,
-                        ),
-                        // Icon()
-                      ],
+                      ),
+                      onPressed: () {},
+                      child: Text('Email'),
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 10, left: 30),
-                    child: Text(
-                      'Hello , welcome back to our account !',
-                      style: TextStyle(
-                          color: Colors.grey, fontWeight: FontWeight.w400),
+                    padding: const EdgeInsets.only(left: 0, top: 60),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(0),
+                        minimumSize: MaterialStateProperty.all(
+                          Size(150, 50),
+                        ),
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            ),
+                            side: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Email',
+                        style: TextStyle(color: Colors.black),
+                      ),
                     ),
-                  )
+                  ),
                 ],
-              )
-            ],
-          )
-        ],
+              ),
+            ),
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(top: 30, right: 12),
+                width: 300,
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        labelText: 'Email address',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        labelText: 'Email address',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
